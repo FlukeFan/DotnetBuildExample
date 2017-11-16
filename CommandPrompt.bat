4e169517b7bb44ceaaadde345238735a3e6fbff6
Richard Brown <flukefan@googlemail.com>
2017-11-16 17:05:31 +0000

    Changed FilterTest parameter to FilterTestFqn

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -3,8 +3,8 @@
 @SET PATH=C:\Program Files\dotnet\;%PATH%
 type readme.txt
 @doskey bc=dotnet clean
-@doskey btw=dotnet watch msbuild Build.csproj /p:FilterTest=$1
-@doskey bt=dotnet msbuild Build.csproj /p:FilterTest=$1
+@doskey btw=dotnet watch msbuild Build.csproj /p:FilterTestFqn=$1
+@doskey bt=dotnet msbuild Build.csproj /p:FilterTestFqn=$1
 @doskey bw=dotnet watch msbuild Build.csproj $*
 @doskey b=dotnet msbuild Build.csproj $*
 @doskey br=dotnet restore Build.csproj $*
